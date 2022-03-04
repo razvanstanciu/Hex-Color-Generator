21c9173becc74744ebc172a72c89ec59ff5d2037
Stanciu Razvan <razvanstanciuba@gmail.com>
2022-03-04 18:25:52 +0200

    adding style to btns

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const span = document.querySelector('span');
-const colors = ['red', 'blue', 'yellow', 'green', 'white', 'brown', 'magenta']
-const clickMe = document.querySelector('#click-me')
-const bottomContainer = document.querySelector('.bottom-container')
+const colors = ['red', 'blue', 'yellow', 'green', 'white', 'brown', 'magenta'];
+const clickMe = document.querySelector('#click-me');
+const bottomContainer = document.querySelector('.bottom-container');
 let counter = 0;
+
+const randomHex = document.querySelector('#random-hex');
+const button = document.querySelector('#random-hex button');
+
+const colorFlipper = document.querySelector('#color-flipper');
+const firstButton = document.querySelector('#color-flipper button')
 
 clickMe.addEventListener('click', function() {
 
@@ -15,5 +21,14 @@ clickMe.addEventListener('click', function() {
     span.innerText = colors[counter];
     bottomContainer.style.background = colors[counter];
 
-   
+});
+
+randomHex.addEventListener('click', function() {
+    button.classList.add('active');
+    firstButton.classList.remove('active');
+});
+
+colorFlipper.addEventListener('click', function() {
+    firstButton.classList.add('active');
+    button.classList.remove('active');
 });
