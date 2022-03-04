abc445b083327573731829ad4ad9bad0e745d322
Stanciu Razvan <razvanstanciuba@gmail.com>
2022-03-04 18:43:44 +0200

    finished project

--- script.js
+++ script.js
@@ -10,25 +10,37 @@ const button = document.querySelector('#random-hex button');
 const colorFlipper = document.querySelector('#color-flipper');
 const firstButton = document.querySelector('#color-flipper button')
 
-clickMe.addEventListener('click', function() {
-
-    if(counter < colors.length - 1) {
-        counter++;
-    } else {
-        counter = 0;
-    }
-    
-    span.innerText = colors[counter];
-    bottomContainer.style.background = colors[counter];
-
-});
 
 randomHex.addEventListener('click', function() {
     button.classList.add('active');
     firstButton.classList.remove('active');
+
 });
 
 colorFlipper.addEventListener('click', function() {
     firstButton.classList.add('active');
     button.classList.remove('active');
+});
+
+clickMe.addEventListener('click', function() {
+
+    if (firstButton.classList.contains('active')) {
+        if(counter < colors.length - 1) {
+            counter++;
+        } else {
+            counter = 0;
+        }
+        
+        span.innerHTML = colors[counter];
+        bottomContainer.style.background = colors[counter];
+    } else {
+        const setBg = () => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            bottomContainer.style.backgroundColor = "#" + randomColor;
+            span.innerHTML = "#" + randomColor;
+          }
+          
+          setBg();
+    };
+
 });
